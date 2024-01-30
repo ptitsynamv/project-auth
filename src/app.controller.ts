ce928a4216f812e0b2537b47b7a1fef45814144a
Mariia Ptytsyna <ptitsynamv@gmail.com>
2024-01-30 12:54:53 +0200

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { OAuth2Client } from 'google-auth-library';
+
+const client = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.CALLBACK_URL,
+);
 
 @Controller()
 export class AppController {
@@ -8,5 +15,25 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/login')
+  async login(@Body('token') token): Promise<any> {
+    const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+
+    const { email, name, picture } = ticket.getPayload();
+
+    return {
+      data: {
+        token: Math.random().toString(36) + Math.random().toString(36),
+        email,
+        name,
+        picture,
+      },
+      message: 'success',
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.GOOGLE_CLIENT_APP_PORT || 3001;
+
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: `http://localhost:${port}`,
+  });
   await app.listen(3000);
 }
 bootstrap();
